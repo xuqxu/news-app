f2d2852b32500b49b062dd069097c51fca3fd56d
xuqxu <froze666@me.com>
2021-08-14 07:55:27 +0900

    コンポーネント化

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, StyleSheet, View, useColorScheme, Image } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import ListItem from './components/ListItem';
 
 export default function App() {
   const colorScheme = useColorScheme();
@@ -13,30 +14,11 @@ export default function App() {
   return (
     <View style={[styles.container, themeContainerStyle]}>
       <StatusBar style="auto" />
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{
-              uri: 'https://picsum.photos/200/200',
-            }}
-          />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={3} style={[styles.text, themeTextStyle]}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
-          <Text style={styles.SubText}>
-            React News Color scheme: {colorScheme}
-          </Text>
-        </View>
-      </View>
+      <ListItem
+        imageUrl="https://picsum.photos/200/200"
+        auther="ReactNews"
+        title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      />
     </View>
   );
 }
@@ -58,27 +40,5 @@ const styles = StyleSheet.create({
   },
   darkThemeText: {
     color: '#d0d0c0',
-  },
-  itemContainer: {
-    height: 100,
-    width: '100%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    flexDirection: 'row',
-  },
-  leftContainer: {
-    width: 100,
-  },
-  rightContainer: {
-    flex: 1,
-    padding: 10,
-    justifyContent: 'space-between',
-  },
-  text: {
-    fontSize: 16,
-  },
-  SubText: {
-    fontSize: 12,
-    color: 'gray',
   },
 });
---
+++ components/ListItem.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Text, StyleSheet, View, useColorScheme, Image } from 'react-native';
+
+export default function ListItem({ imageUrl, title, auther }) {
+  const colorScheme = useColorScheme();
+
+  const themeTextStyle =
+    colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
+
+  return (
+    <View style={styles.itemContainer}>
+      <View style={styles.leftContainer}>
+        <Image
+          style={{ width: 100, height: 100 }}
+          source={{
+            uri: imageUrl,
+          }}
+        />
+      </View>
+      <View style={styles.rightContainer}>
+        <Text numberOfLines={3} style={[styles.text, themeTextStyle]}>
+          {title}
+        </Text>
+        <Text style={styles.SubText}>
+          {auther} Color scheme: {colorScheme}
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  lightThemeText: {
+    color: '#242c40',
+  },
+  darkThemeText: {
+    color: '#d0d0c0',
+  },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+  },
+  SubText: {
+    fontSize: 12,
+    color: 'gray',
+  },
+});
